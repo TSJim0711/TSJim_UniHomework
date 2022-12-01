2d15176ee9236fce78208065c5e0b06548846730
TSJim0711 <74333176+TSJim0711@users.noreply.github.com>
2022-12-01 12:11:31 +0800

    Add files via upload

---
+++ 实验3数组、结构体及指针/ConsoleApplication3.cpp
@@ -0,0 +1,14 @@
+﻿/*1、 要求使用指针处理下面的问题，输入四个整数，按由小到大的顺序输出；然后将程序改为：输入四个字符串，按由小到大顺序输出。（使用指针或指针数组）*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string aString, bString, cString;
+	cout << "a:";
+	cin >> aString; 
+	cout << "b:";
+	cin>> bString;
+	cString = bString + aString;
+	cout <<"c:" << cString;
+}
