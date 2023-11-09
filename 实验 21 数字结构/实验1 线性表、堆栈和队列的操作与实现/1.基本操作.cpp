9aa8e2107ab1101921359a59d0e0821f80ab71aa
TSJim0711 <74333176+TSJim0711@users.noreply.github.com>
2023-11-09 23:00:40 +0800

    Update 1.基本操作.cpp
    我感觉自己可以！

--- 实验 21 数字结构/实验1 线性表、堆栈和队列的操作与实现/1.基本操作.cpp
+++ 实验 21 数字结构/实验1 线性表、堆栈和队列的操作与实现/1.基本操作.cpp
@@ -65,9 +65,100 @@ void Destroy(SNode*& top)
 
 int main()
 {
-    SNode* top;
+    SNode* top,* current;
     DataType item=10;
     InitStack(top);
-    Push(&top, item);
-    GetTop(top, item);
+    int op, userInput, userInput2;
+    //随机数入链表
+    srand(time(0));
+    for (int i = 1; i <= 10; i++)
+        Push(&top, rand() % 900 + 100);
+    //Manu
+    while (true)
+    {
+        cout << "Input\tOperation\n[1]\tPrint Stack\n[2]\tStack lookup\n[3]\tInsert data & print\nOperation [ ]\b\b";
+        if (!(cin >> op))
+            op= -1;
+        switch (op)
+        {
+        case 1://输出链表内容
+            current = top;
+            while (current != NULL)
+            {
+                cout << current->data << "\t";
+                current = current->next;
+            }
+            cout << endl;
+            break;
+        case 2://读入数，在？在，输出位置
+            current = top;
+            cout << "Input your number (integers only) : ";
+            if (!(cin >> userInput))
+            {
+                cout << "Input rejected. Integers only";
+                break;
+            }
+            for (int i = 1; current != NULL; i++)
+            {
+                if (current->data == userInput)
+                    cout << "Found! In " << i<<endl;
+                current = current->next;
+            }
+            cout << "End of seek. If no \"Found\" context, your input do not exist in stack."<<endl;
+            break;
+        case 3: //读入数+位置，插入数在位置，输出链表
+            cout << "Input location you like to insert (integers only) : ";
+            if (!(cin >> userInput))
+            {
+                cout << "Input rejected. Integers only.";
+                break;
+            }
+            current = top;
+            for (int i = 1; i <= userInput - 2 ; i++)
+            {
+                if (current != NULL)
+                {
+                   current = current->next;
+                }
+                else    
+                {
+                    cout << "Invalid location input, as exceed stack range.";
+                    break;
+                }
+            }
+            cout << "Input data you like to insert (integers only) : ";
+            if (!(cin >> userInput2))
+            {
+                cout << "Input rejected. Integers only.";
+                break;
+            }
+            SNode* temp;
+            temp = (SNode*)malloc(sizeof(SNode));
+            temp->data = userInput2;
+            if(userInput==1) //1是特别的！要对top操作！！！！
+            {
+                temp->next = top->next;
+                top = temp;
+            }else
+            {
+                temp->next = current->next;
+                current->next = temp;
+            }
+            //输出链表内容，是照搬的
+            current = top;
+            while (current != NULL)
+            {
+                cout << current->data << "\t";
+                current = current->next;
+            }
+            cout << endl;
+            break;
+        default: 
+            cout << "Input rejected, invalid operation code."<<endl;
+            break;
+        }
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    
 }
