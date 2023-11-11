bb169cf4a9ecec6829f78ed80fb374c4857bd675
TSJim0711 <74333176+TSJim0711@users.noreply.github.com>
2023-11-11 15:22:48 +0800

    Update 1.基本操作.cpp
    Case Closed!

--- 实验 21 数字结构/实验1 线性表、堆栈和队列的操作与实现/1.基本操作.cpp
+++ 实验 21 数字结构/实验1 线性表、堆栈和队列的操作与实现/1.基本操作.cpp
@@ -1,3 +1,13 @@
+/*
+1、线性表的链表实现：
+（1）用随机函数生成10个3位整数（100~999），把这些整数存于链表中；
+（2）输出链表的内容；
+（3）读入一个整数，查看该整数是否在表中，若在，输出其位置（首位置为1）；
+（4）读入一个整数，以及要插入的位置，把该整数插入到链表中，输出链表的内容（要求判断输入的位置是否合理）；
+（5）读入一个整数，若该整数在链表里，删除该整数，输出链表的内容；
+（6）把链表的内容翻转，输出链表的内容。
+*/
+
 #include <iostream>
 #include <ctime>
 
@@ -65,7 +75,7 @@ void Destroy(SNode*& top)
 
 int main()
 {
-    SNode* top,* current,*B4Current;
+    SNode* top,* current,*B4Current,*AfterCurrent;
     DataType item=10;
     InitStack(top);
     int op, userInput, userInput2;
@@ -76,7 +86,7 @@ int main()
     //Manu
     while (true)
     {
-        cout << "Input\tOperation\n[1]\tPrint Stack\n[2]\tStack lookup\n[3]\tInsert data & print\nOperation [ ]\b\b";
+        cout << "Input\tOperation\n[1]\tPrint Stack\n[2]\tStack lookup\n[3]\tInsert data & print\n[4]\tDelete data & print\n[5]\tFlip stack & print\nOperation [ ]\b\b";
         if (!(cin >> op))
             op= -1;
         switch (op)
@@ -110,7 +120,7 @@ int main()
             cout << "Input location you like to insert (integers only) : ";
             if (!(cin >> userInput))
             {
-                cout << "Input rejected. Integers only.";
+                cout << "Input rejected. Integers only.\n";
                 break;
             }
             current = top;
@@ -122,14 +132,14 @@ int main()
                 }
                 else    
                 {
-                    cout << "Invalid location input, as exceed stack range.";
+                    cout << "Invalid location input, as exceed stack range." << endl;
                     break;
                 }
             }
             cout << "Input data you like to insert (integers only) : ";
             if (!(cin >> userInput2))
             {
-                cout << "Input rejected. Integers only.";
+                cout << "Input rejected. Integers only.\n";
                 break;
             }
             SNode* temp;
@@ -153,13 +163,13 @@ int main()
             }
             cout << endl;
             break;
-        case 4:
+        case 4: //
             current = top;
             B4Current = current;
             cout << "Input your number (integers only) : ";
             if (!(cin >> userInput))
             {
-                cout << "Input rejected. Integers only";
+                cout << "Input rejected. Integers only."<<endl;
                 break;
             }
             for (int i = 1; current != NULL; i++)
@@ -172,16 +182,45 @@ int main()
                     temp = current;
                     current = current->next;
                     free(temp);
-                }
-                else
+                    if (i==1)//只有删除位置1时触发，因为temp(同时也是top)已free，重定位top至正确
+                        top = current;
+                } else//如果没有找到，就一般推进
                 {
                     B4Current = current;
                     current = current->next;
                 }
-
             }
+            // 输出链表内容，也是照搬的
+                current = top;
+            while (current != NULL)
+            {
+                cout << current->data << "\t";
+                current = current->next;
+            }
+            cout << endl;
+            break;
+        case 5:
+            AfterCurrent = top;
+            B4Current = current = NULL;
+            do
+            {
+                B4Current = current;//这是B4(A)=>Now(B)=>Aft(C)=>C.next(D)
+                current = AfterCurrent;
+                AfterCurrent = AfterCurrent->next;//由B4(B)->Now(C)->Aft(D)->D.next(E)
+                current->next = B4Current;//变成B4(B)<-Now(C)->Aft(D)->D.next(E)
+            } while (AfterCurrent != NULL);
+            top = current;
+            //输出链表内容，同样是照搬的
+            current = top;
+            while (current != NULL)
+            {
+                cout << current->data << "\t";
+                current = current->next;
+            }
+            cout << endl;
+            break;
         default: 
-            cout << "Input rejected, invalid operation code."<<endl;
+            cout << "Input rejected, invalid operation code.\n"<<endl;
             break;
         }
         cin.clear();
