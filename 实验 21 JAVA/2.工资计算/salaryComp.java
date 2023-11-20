dd36378d9a71ce7e5c243da2f4d3ac1ae0d15b9c
TSJim0711 <74333176+TSJim0711@users.noreply.github.com>
2023-11-20 10:04:43 +0800

    Complete salaryComp.java
    good

--- 实验 21 JAVA/2.工资计算/salaryComp.java
+++ 实验 21 JAVA/2.工资计算/salaryComp.java
@@ -1,4 +1,5 @@
 import java.util.Scanner; 
+import java.util.InputMismatchException;
  
 abstract class employee{
 	public abstract void ComputeSalary();	
@@ -9,7 +10,7 @@ class manager extends employee
 {
     public void ComputeSalary(){
         salary = 18000;
-        System.out.println(salary);
+        System.out.println("Salary: "+ salary);
     }
 }
 
@@ -19,16 +20,17 @@ class manager extends employee
     	System.out.println("Salary Bounus: ");
     	Scanner bonus = new Scanner(System.in);
         salary = 2500 + bonus.nextInt();
-        System.out.println(salary);
+        System.out.println("Salary: "+ salary);
     }
 }
 
  class worker extends employee
 {
     public void ComputeSalary(){
+    	System.out.println("Working day(s): ");
     	Scanner workingDay = new Scanner(System.in);
         salary =  workingDay.nextInt()*68;
-        System.out.println(salary);
+        System.out.println("Salary: "+ salary);
     }
 }
 
@@ -37,14 +39,24 @@ public class salaryComp {
 		manager mng = new manager();
 		sales sale = new sales();
 		worker wkr = new worker();
-	for(int i =1; i==1;)
-	{
-		System.out.println("Employee to compute: [1] Manager [2] Salesman [3] Worker");
-    	Scanner employeeClass = new Scanner(System.in);
-    	if(employeeClass.toString()=="1")
-    		mng.ComputeSalary();
-    	if(employeeClass.toString()=="2")
-    		sale.ComputeSalary();
-    	if(employeeClass.toString()=="3")
-    		wkr.ComputeSalary();
+		Scanner userInput;
+		int employeeClass;
+		do
+		{
+			System.out.println("Employee to compute: [1] Manager [2] Salesman [3] Worker [4] Exit;");
+			userInput = new Scanner(System.in);
+			try {employeeClass = userInput.nextInt();}
+			catch(InputMismatchException e )
+			{
+				System.out.println("Numbers only.");
+				employeeClass=0;
+			}
+	    	if(employeeClass==1)
+	    		mng.ComputeSalary();
+	    	else if(employeeClass==2)
+	    		sale.ComputeSalary();
+	    	else if(employeeClass==3)
+	    		wkr.ComputeSalary();
+		}while (employeeClass<=3);
 	}
+}
