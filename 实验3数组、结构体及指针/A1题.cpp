ed6a4bf15bec7289cedf80e121133bf1eed7873e
TSJim0711 <74333176+TSJim0711@users.noreply.github.com>
2023-12-08 18:05:18 +0800

    Update A1题.cpp, able to destroy graph

--- 实验3数组、结构体及指针/A1题.cpp
+++ 实验3数组、结构体及指针/A1题.cpp
@@ -4,74 +4,144 @@
 （3）打印出名次表，表格内包括学生编号、各科分数、总分和平均分；
 （4）任意输入一个学号，能够查找出该学生在班级中的排名及其考试分数。
 */
-
 #include <iostream>
+
 using namespace std;
-
-int stuID[31], mathScore[31], engScore[31], phyScore[31], averageScore[31], fullScore[31], highestScore;
-
-int showIntel(int j)
+typedef struct ANode
 {
-    cout << "\nStudent ID: " << stuID[j] << "\nMath score: " << mathScore[j] << "\nEnglish Score:" << engScore[j] << "\nPhysic Score:" << phyScore[j] << "\nFull mark:" << fullScore[j] << "\nAverage:" << averageScore[j] << "\n---------------------";
-    return(j);
+    int adjvex;
+    struct ANode* nextarc;
+    int weight;
+}ArcNode;
+
+typedef struct Vnode
+{
+    int data;
+    ArcNode* firstarc;
+}VNode;
+
+typedef struct AdjGraph
+{
+    VNode adjlist[20];//邻接表，最大20
+    int n ,e;////图中顶点数n和边数e
+}AdjGraph;
+
+AdjGraph* createAdjGraph(int *graphRq)
+{
+    AdjGraph *mainTrunk = (AdjGraph*)malloc(sizeof(AdjGraph));
+    VNode *pointerA = NULL, *pointerB = NULL;
+    VNode* VNodeTemp;
+    ANode* ANodeTemp, * ANodePointer;
+    mainTrunk->e = mainTrunk->n = 0;
+    bool doubleInitialize = false;
+    int emptySample = mainTrunk->adjlist[1].data;
+    int loopFlag = -1;//loop flag 相当于do-while loop 的计数器
+    for (int i = 0; i <= sizeof(graphRq); i++)
+    {
+        do
+        {
+            loopFlag ++;
+            if (mainTrunk->adjlist[loopFlag].data == graphRq[i*3])
+                pointerA = &mainTrunk->adjlist[loopFlag];
+            if (mainTrunk->adjlist[loopFlag].data == graphRq[i*3+1])
+                pointerB = &mainTrunk->adjlist[loopFlag];
+        } while (mainTrunk->adjlist[loopFlag].data != emptySample && (pointerA == NULL || pointerB == NULL));//寻找节点对应的VNode，没有就跳出。这里的data很吊诡，不是NULL,0,我不知道如何判断，只能透过emptySample
+//        if(pointerB == NULL && pointerB == pointerA)
+//            pointerB = &mainTrunk->adjlist[loopFlag+1];
+        ANodeTemp = (ANode*)malloc(sizeof(ANode));
+        ANodeTemp->adjvex = graphRq[i * 3 + 1];//先准备好要插入的新的node的内容
+        ANodeTemp->nextarc = NULL;
+        ANodeTemp->weight = graphRq[i * 3 + 2];
+        if (pointerA == NULL)//代表do-while loop中没有找到对应的开始顶点
+        {
+            pointerA = &mainTrunk->adjlist[loopFlag];//赋予新行作为开始顶点
+            pointerA->data = graphRq[i * 3];
+            pointerA->firstarc = ANodeTemp;//插入新的node的内容
+            mainTrunk->n++;//图中顶点数n++和边数e++
+            mainTrunk->e++;
+            doubleInitialize = true;//设潜在的双重初始化为真
+        }else//代表do-while loop中找到对应的开始顶点
+        {
+            ANodePointer=pointerA->firstarc;
+            while (ANodePointer->nextarc != NULL)//找到对应顶点的nextarc链的尾
+                ANodePointer = ANodePointer->nextarc;
+            ANodePointer->nextarc = ANodeTemp;//插入的新的node的内容
+            mainTrunk->e++;//图中边数e++
+        }
+        //----A及B结构相同，注释略----//
+        ANodeTemp = (ANode*)malloc(sizeof(ANode));
+        ANodeTemp->adjvex = graphRq[i * 3];
+        ANodeTemp->nextarc = NULL;
+        ANodeTemp->weight = graphRq[i * 3 + 2];
+        if (pointerB == NULL)
+        {
+            pointerB = &mainTrunk->adjlist[loopFlag + (doubleInitialize?1:0)];//如果潜在的双重初始化为真，则代表需要以loopFlag+1行作为开始顶点
+            pointerB->data = graphRq[i * 3 + 1];
+            pointerB->firstarc = ANodeTemp;
+            mainTrunk->n++;//图中顶点数n++和边数e++
+            mainTrunk->e++;
+        }
+        else
+        {
+            ANodePointer = pointerB->firstarc;
+            while (ANodePointer->nextarc != NULL)
+                ANodePointer = ANodePointer->nextarc;
+            ANodePointer->nextarc = ANodeTemp;
+            mainTrunk->e++;
+        }
+        pointerA = pointerB = NULL;
+        doubleInitialize = false;
+        loopFlag = -1;
+    }
+    return mainTrunk;
 }
 
-int switcher(int from, int to)
+void printAdjGraph(AdjGraph* grf)
 {
-    stuID[to] = stuID[from];
-    mathScore[to] = mathScore[from];
-    engScore[to] = engScore[from];
-    phyScore[to] = phyScore[from];
-    averageScore[to] = averageScore[from];
-    fullScore[to] = fullScore[from];
-    return(fullScore[to]);
+    ANode* pointerANode;
+    for (int i = 0; i <= grf->n - 1; i++)//历遍adjlist
+    {
+        cout << grf->adjlist[i].data;
+        pointerANode = grf->adjlist[i].firstarc;
+        while (pointerANode != NULL)//历遍直至nextarc链尾
+        {
+            cout <<" -> ["<< pointerANode->adjvex <<", " << pointerANode->weight<< "]";
+            pointerANode = pointerANode->nextarc;
+        }
+        cout << endl;
+    }
+}
+
+void destroyAdjGraph(AdjGraph* grf)
+{
+    ANode *pointer2Del, *pointer2Handle =NULL;
+    for (int i = 0; i < grf->n; i++)
+    {
+        do
+        {
+            pointer2Del = grf->adjlist[i].firstarc;
+            while (pointer2Del->nextarc != NULL )
+            {
+                if ( pointer2Del->nextarc->nextarc == NULL)//在删除节点的前一个节点放个旗
+                    pointer2Handle = pointer2Del;
+                pointer2Del = pointer2Del->nextarc;//指向删除节点
+            }
+            free(pointer2Del);//删除节点
+            if (pointer2Handle != NULL)
+                pointer2Handle->nextarc = NULL;//把删除节点的前一个节点的nextarc = NULL,方便下一次while loop中判断
+            else
+                grf->adjlist[i].firstarc = NULL;//如果pointer2Handle == NULL,此时adjlist[i]已清空,所以赋adjlist[i]值NULL,方便 do-while loop 判断
+            pointer2Handle = NULL;
+        } while (grf->adjlist[i].firstarc != NULL);
+    }
+    free(grf);
 }
 
 int main()
 {
-    int temp ,counter, i, j, k;
-    cout << "Input student's ID, MATHS score, ENG score and physic score respectively:" << endl;
-    for (i = 1; i <= 30; i++)
-    {
-        cin >> stuID[i] >> mathScore[i] >> engScore[i] >> phyScore[i];
-        fullScore[i] = engScore[i] + mathScore[i] + phyScore[i];
-        averageScore[i] = fullScore[i] / 3;
-        cout << i << "/30 student(s) had been input." << endl << "Input “1” to finish the input process,else input anything: ";
-        cin >> j;
-        if (j == 1) break;
-    }
-
-    for (j = 1; j <= i; j++)
-    {
-        highestScore = fullScore[j];
-        for (k = j; k <= i; k++)
-        {
-            if (fullScore[k] < highestScore)
-            {
-                switcher(k, 31);
-                switcher(j, k);
-                highestScore = switcher(31, j);
-            }
-        }
-    }
-    for (j = 1; j <= i; j++) 
-        showIntel(j);
-
-    for (; i == i;)
-    {
-        cout << "\nEnter a student ID to search:";
-        cin >> temp;
-        counter = 0;
-        for (k = 1; k <= i; k++)
-        {
-            if (temp == stuID[k])
-            {
-                showIntel(k);
-                counter++;
-            }
-        }
-        if (counter == 0)
-            cout << "\nStudent ID not found, please retry.";
-        cout << "\nSearch ended.\n---------------------";
-    }
+    int graphRequest[9][3] = { 0,1,28,6,4,24,1,2,16,2,3,12,3,4,22,4,5,25,5,0,10,1,6,14,6,3,18}; //{线路节点，线路节点，线路权重}
+    AdjGraph *grf;
+    grf = createAdjGraph(*graphRequest);
+    printAdjGraph(grf);
+    destroyAdjGraph(grf);
 }
