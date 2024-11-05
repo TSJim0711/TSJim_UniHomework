793eead5ec5e721ca8fab76aeed8516be997272a
TSJim0711 <74333176+TSJim0711@users.noreply.github.com>
2024-11-05 16:50:02 +0800

    Update 线性回归
    I'm lost.........

--- 实验 31 机器学习及数据分析/311 线性回归/main.py
+++ 实验 31 机器学习及数据分析/311 线性回归/main.py
@@ -1,20 +1,29 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 data = pd.read_csv("housing.csv")
+ocean_proximity_map={"INLAND":0,"<1H OCEAN":1,"NEAR OCEAN ":2,"NEAR BAY":3,"ISLAND":4}
+data['ocean_proximity']=data['ocean_proximity'].map(ocean_proximity_map)#ocean_proximity hot-one encode
+data=data.dropna() #drop column with empty cell
 
-data_encoded=pd.get_dummies(data,columns=['ocean_proximity'])
+dataEncoded=pd.get_dummies(data)
+trainSet, testSet=train_test_split(dataEncoded, test_size=0.3, random_state=42)
 
-s=(data.dtypes=='ocean_proximity')
-object_cols=list(s[s].index)
+X = trainSet.drop(columns=['median_house_value']).values
+Y = trainSet['median_house_value'].values
+XTest = testSet.drop(columns=['median_house_value']).values
+YTest = testSet['median_house_value'].values
 
-X = data_encoded.drop(columns=['median_house_value']).values.astype(np.float64)
-Y=data_encoded['median_house_value'].values
+X=np.matrix(trainSet.drop(columns=['median_house_value']).values) #convert to matrix
+Y=np.transpose(np.matrix(trainSet['median_house_value'].values))#rotate Y from 1*n to n*1
+XTest=np.matrix(testSet.drop(columns=['median_house_value']).values)
+YTest=np.matrix(testSet['median_house_value'].values)
 
-W=np.linalg.inv(X.T@X)@X.T@Y
+print("X:",X.shape)
+print("Y:",Y.shape)
 
-pd.set_option('display.max_rows',5)
-pd.set_option('display.max_columns',None)
-
-print("data_Enc:\n",data_encoded)
-print("W:",W)
+#特征缩放
+sigma=np.std(X,axis=0)#X的每列标准差
+mu=np.mean(X.mean,axis=0)#X的每列平均
+X=(x-mu)/sigma
